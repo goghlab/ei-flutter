6ccc86bd7ed34c214f9f5dc00b47d2755eabcb6a
izzy <dom851103@gmail.com>
2023-12-18 23:01:16 -0800

    initialize firebaseconfig in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
 import 'package:xam_shoes_app/core/constants/string_constants.dart';
 import 'package:xam_shoes_app/core/constants/theme_constants.dart';
 import 'package:xam_shoes_app/core/controllers/auth/auth_controller.dart';
@@ -18,8 +17,24 @@ import 'package:xam_shoes_app/core/controllers/theme/theme_controller.dart';
 import 'package:xam_shoes_app/core/controllers/track_location/track_location_controller.dart';
 import 'package:xam_shoes_app/core/translations/languages.dart';
 import 'package:xam_shoes_app/ui/views/splash/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart'; // Import Firebase Core
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyCG5pVt5fXPgHUeqpsxbpTt702cg6leJKU",
+      authDomain: "ei-website-103a4.firebaseapp.com",
+      databaseURL: "https://ei-website-103a4-default-rtdb.asia-southeast1.firebasedatabase.app",
+      projectId: "ei-website-103a4",
+      storageBucket: "ei-website-103a4.appspot.com",
+      messagingSenderId: "609909198676",
+      appId: "1:609909198676:web:88fe2d7d886e6eb17202c1",
+      measurementId: "G-HZQ5QYRXQX",
+    ),
+  );
+
   Get
     ..put(AuthController())
     ..put(NavigationController())
@@ -34,6 +49,7 @@ void main() {
     ..put(MessageController())
     ..put(SettingsController())
     ..put(TrackLocationController());
+  
   runApp(const MyApp());
 }
 
